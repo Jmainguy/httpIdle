5aab4ab7f051e666e77378046f99360a3128321b
Jonathan Mainguy <jon@soh.re>
2020-10-22 10:14:06 -0400

    show when connections happen

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Connection received")
 
-    fmt.Fprintf(w, "hello\n")
-    time.Sleep(60 * time.Second)
+	fmt.Fprintf(w, "hello\n")
+	time.Sleep(60 * time.Second)
 }
 
 func main() {
 
-    http.HandleFunc("/", hello)
-
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
