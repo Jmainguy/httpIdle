78b64ed04e42a0dfa7cdc925bfa7b67a12bc11e5
John Kost <jkost@bandwidth.com>
2020-10-23 10:57:57 -0400

    Set idle to 10

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Connection received")
 
 	fmt.Fprintf(w, "hello\n")
-	time.Sleep(60 * time.Second)
+	time.Sleep(10 * time.Second)
 }
 
 func main() {
